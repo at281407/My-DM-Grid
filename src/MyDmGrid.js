e60731d65337ffed68999420a424570c64d467a7
Andrew Taylor <ataylor@caleres.com>
2018-11-27 07:43:08 -0600

    MyDmGrid ini commit

---
+++ src/MyDmGrid.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class MyDmGrid extends Component {
+    render(){
+        return(
+            <div className="mainView">MAIN VEW HERE</div>
+        )
+    }
+}
+
+export default MyDmGrid
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+function App() {
+    return (
+    <div className="App">
+        <header className="App-header">
+        <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Learn React
+        </a>
+        </header>
+    </div>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
