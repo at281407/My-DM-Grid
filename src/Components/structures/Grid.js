750eea3668d888417ab5532096d982da0eca5bd8
Andrew Taylor <ataylor@caleres.com>
2018-12-10 08:05:24 -0600

    Cards init

--- src/Components/structures/Grid.js
+++ src/Components/structures/Grid.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
 
-class Grid extends Component {
-  render () {
-    return <div></div>
-  }
-}
+import Card from "./Card.js";
 
-export default Grid
+class Grid extends React.Component {
+  
+    initGrid = () => {
+      var g = [];
+      //props.state.grid = new Array(this.props.width);
+      for(var i = 0; i < this.props.width; i++){
+        g[i] = new Array(this.props.height);
+        for(var j = 0; j < this.props.height; j++){
+          var cellClass = "gridCell row-"+i+"-col-"+j;
+          g[i][j] = cellClass;
+        }
+      }
+      return g;
+    };
+  
+    getInitCords = (x,y) => {
+      var cords = {
+        x: x,
+        y: y
+      }
+      return cords;
+    }
+  
+    render(){
+      
+      return (
+        <h1>TITLE</h1>
+      );
+    }
+  }
+
+export default Grid;
--- src/Components/views/CurrGrid/index.js
+++ src/Components/views/CurrGrid/index.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 
+import Grid from "../../structures/Grid";
+import Card from "../../structures/Card";
+
 import {CurrGrid} from "../../_styles/views/Curr-Grid"
 
 class CurrGridView extends Component {
   render () {
-    return <CurrGrid><h1>GRID VIEW</h1></CurrGrid>;
+    return (
+      <CurrGrid>
+         <Grid />
+         <Card />
+      </CurrGrid>
+    );
   }
 }
 
